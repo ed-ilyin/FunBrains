b5ec252378ced3964fdda7c829e15ae7e396d644
Ed Ilyin <ed.il@live.com>
2017-08-24 12:21:12 +0300

    tried examples working

--- FunBrains.fsx
+++ FunBrains.fsx
@@ -9,9 +9,11 @@ type Core =
 type Term =
     | Symbol of Symbol
     | Apply of Term * Term
-    | Abstract of Symbol * Term
+    | Lambda of Symbol * Term
     | Core of Core
 
+
+type Command = Stop | TryAgain
 
 let tuple x y = x, y
 
@@ -19,13 +21,14 @@ let tuple x y = x, y
 let rec sprint =
     function
         | Symbol x -> sprintf "%s" x
-        | Apply (f, x) -> sprint x |> sprintf "%s (%s)" (sprint f)
-        | Abstract (x, t) -> sprint t |> sprintf "%s -> %s" x
+        | Apply (f, x) -> sprint x |> sprintf "(%s %s)" (sprint f)
+        | Lambda (x, t) -> sprint t |> sprintf "(λ%s. %s)" x
         | Core x -> sprintf "%A" x
 
 
 let log tag x =
-    do sprint x |> printfn "%s: %s" tag
+    // do sprint x |> printfn "%s: %s" tag
+    do printfn "%s: %A" tag x
     x
 
 
@@ -44,61 +47,59 @@ let rec alpha symbol =
 
         | Apply (f, x) -> Apply (alph f, alph x)
 
-        | Abstract (s, t) ->
+        | Lambda (s, t) ->
             if s = symbol
                 then rename s
                 else s
-                |> (alph t |> flip tuple >> Abstract)
+                |> (alph t |> flip tuple >> Lambda)
 
         | Core x -> Core x
 
-let rec apply argument =
-    let rec beta symbol =
-        let bet term = beta symbol term
 
-        function
-            | Symbol x -> if x = symbol then argument else Symbol x 
-            | Apply (f, t) -> bet t |> apply <| bet f
+let rec beta symbol argument =
+    let bet term = beta symbol argument term
 
-            | Abstract (s, t) ->
-                match s = symbol, Symbol s = argument with
-                    | false, false -> s, bet t
-                    | false, true -> rename s, alpha s t |> bet
-                    | true, _ -> s, t
-                    |> Abstract
-                    
-            | Core x -> Core x
+    function
+        | Symbol x -> if x = symbol then argument else Symbol x 
+        | Apply (f, t) -> Apply (bet f, bet t)
 
-    argument
-        |> sprint
-        |> sprintf "apply to %s of"
-        |> log
-        >> function
-        | Symbol x -> Apply (Symbol x, argument)
-
-        | Apply (f, x) -> f |> apply x |> functionapply argument
-        
-        | Abstract (x, t) -> beta x t
+        | Lambda (s, t) ->
+            match s = symbol, Symbol s = argument with
+                | false, false -> s, bet t
+                | false, true -> rename s, t |> alpha s |> bet
+                | true, _ -> s, t
+                |> Lambda
+                
         | Core x -> Core x
 
 
 let rec run =
     function
-        | Symbol x -> Symbol x
-        | Apply (f, x) -> f |> apply x
-        | Abstract (x, t) -> Abstract (x, run t)
-        | Core x -> Core x
+        | Symbol x -> Symbol x, Stop
+        | Apply (Lambda (s, t), a) -> t |> beta s a |> run
+
+        | Apply (f, x) ->
+            match run f, run x with
+                | (f, Stop), (x, _) -> Apply (f, x), Stop
+                | (f, _), (x, _) -> Apply (f, x) |> run
+
+        | Lambda (x, t) ->
+            let t, _ = run t
+            Lambda (x, t), TryAgain
+
+        | Core x -> Core x, Stop
 
 
 let curry f x y = f (x, y)
 let (!) = Symbol
 let (*) = curry Apply
-let (:=) = curry Abstract
+let (:=) = curry Lambda
 let int = Int >> Core
 // let term = ("z" := ("x" := !"x" * !"z") * !"x") * (("y" := !"x" * !"y") * int 5)
-// let term = ("w" := "1" := !"w" * !"1") * ("x" := !"x" * !"x") * ("y" := "z" := !"y" * !"z")
 // let term = ("x" := "x'" := !"x" * !"x'") * ("x" := !"x" * !"x") * ("x" := "x'" := !"x" * !"x'")
 // let term = ("x" := !"x" * !"x") * ("y" := ("z" := !"y" * !"z"))
 // let term = Int 5 |> Core
-let term = ("z" := (!"z" * (!"y" * !"z"))) * (!"z" * !"x") * !"z"
-sprint term, run term |> sprint
+let term = ("w" := "1" := !"w" * !"1") * ("x" := !"x" * !"x") * ("y" := "z" := !"y" * !"z")
+// let term = ("z" := (!"z" * (!"y" * !"z"))) * (!"z" * !"x") * !"z"
+let t, cmd = run term
+sprint term, sprint t, cmd
